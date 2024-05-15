0a9bb942719ef5ac983d5cf1eb483bfde1d62e71
Daaanilo <145839755+Daaanilo@users.noreply.github.com>
2024-05-15 16:15:07 +0200

    Add files via upload

--- jsonifydiscovery.py
+++ jsonifydiscovery.py
@@ -2,8 +2,7 @@ import os
 import json
 import argparse
 import types
-
-from classes.concrete_loader import ConcreteLoader
+import importlib
 
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -31,31 +30,53 @@ def save_results_to_json(discovery_infos, save_path):
     except Exception as e:
         print(f"Si è verificato un errore durante il salvataggio del file: {str(e)}")
 
+def load_json_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except Exception as e:
+        print(f"Errore durante il caricamento del file JSON {file_path}: {str(e)}")
+        return None
+
 def main():
     parser = argparse.ArgumentParser(
         prog='jsonifydiscovery.py',
         description='Analizza file e genera in output un file JSON.'
     )
-    parser.add_argument('csv_file', help="File CSV.")
-    parser.add_argument('log_files', nargs='+', help="File di log.")
+    parser.add_argument('-d', '--csv_file', help="File CSV.", required=True)
+    parser.add_argument('-f', '--log_files', nargs='+', help="File di log.", required=True)
     parser.add_argument('-o', '--output', help="Output file JSON.", type=str, default="result.json")
+    parser.add_argument('-a', '--algorithmFile', help="File dell'algoritmo JSON.", type=str)
+    parser.add_argument('-e', '--executionInfoFile', help="File di informazioni sull'esecuzione JSON.", type=str)
+    parser.add_argument('-l', '--loaderClass', help="Nome della classe del loader.", type=str, required=True)
     args = parser.parse_args()
 
     csv_file = args.csv_file
     log_files = args.log_files
+    algorithm_data = args.algorithmFile
+    execution_info_data = args.executionInfoFile
+    loader_class_name = args.loaderClass
 
-    if not log_files:
-        print("Nessun file di log specificato.")
+    module_name, class_name = loader_class_name.rsplit('.', 1)
+    try:
+        module = importlib.import_module(module_name)
+        loader_class = getattr(module, class_name)
+    except (ImportError, AttributeError) as e:
+        print(f"Errore durante il caricamento della classe {loader_class_name}: {e}")
         return
 
     discovery_infos = []
     for log_file in log_files:
-        loader = ConcreteLoader(executionFile=log_file, datasetFile=csv_file, algorithmFile="", executionInfoFile="")
+        loader = loader_class(
+            executionFile=log_file, 
+            datasetFile=csv_file, 
+            algorithmFile=algorithm_data, 
+            executionInfoFile=execution_info_data
+        )
         discovery_info = loader.loadDiscoveryInfo()
         discovery_infos.append(discovery_info)
 
     save_results_to_json(discovery_infos, args.output)
-
 
 if __name__ == "__main__":
     main()
