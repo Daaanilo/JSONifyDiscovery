73a61ddab241615c0fb4771fd7c689fe69709db7
Daaanilo <145839755+Daaanilo@users.noreply.github.com>
2024-05-23 15:58:59 +0200

    Delete classes directory

--- classes/execution/result/data/attribute.py
+++
@@ -1,7 +0,0 @@
-class Attribute:
-    column: str
-    comparison_relaxation: float
-
-    def __init__(self, column, comparison_relaxation):
-        self.column = column
-        self.comparison_relaxation = comparison_relaxation
